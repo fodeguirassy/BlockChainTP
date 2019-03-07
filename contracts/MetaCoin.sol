af1ac080cf1ff43f42febeacbb3c5eb10b0aad2b
Fodé Guirassy <fode.guirassy@ekino.com>
2019-03-07 18:34:56 +0100

    add function to add mocked data

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -10,11 +10,44 @@ import "./ConvertLib.sol";
 contract MetaCoin {
     mapping (address => uint) balances;
 
+    Owner benjamin;
+
+    mapping (address => Owner) public houseOwners;
+    mapping (int => int[]) public housesToOwners;
+
+    House house1;
+    House house2;
+    House house3;
+    House house4;
+    House house5;
+
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     constructor() public {
         balances[msg.sender] = 10000;
+
+        //Add mocked Data
+        addBenjaminHouses(0xC1991F924F713aAC384d63cdb5fdf7060C72a669, "Benjamin", 1000000);
     }
+
+    function addBenjaminHouses(address ownerAddr, string memory _name, int _money) public {
+      house1 = House(1,"12 rue Fragonard, Nice", 120000, false, false);
+      house2 = House(2,"13 rue Chanzy, Paris", 160000, false, false);
+      house3 = House(4,"16 Villa Marguerite, Boulogne", 187000, false, false);
+      house4 = House(5,"26 rue Marcel Icard, Paris", 1200000, false, false);
+      house5 = House(6,"45 rue Farafagné, Lyon", 220000, false, false);
+
+      int[] memory ids;
+      ids[0] = 1;
+      ids[1] = 2;
+      ids[2] = 3;
+      ids[3] = 4;
+
+      benjamin = Owner(ownerAddr, _name, _money, ids);
+
+      houseOwners[ownerAddr] = benjamin;
+    }
+
 
     function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
         if (balances[msg.sender] < amount) return false;
@@ -30,5 +63,20 @@ contract MetaCoin {
 
     function getBalance(address addr) public view returns(uint) {
         return balances[addr];
+    }
+
+    struct Owner {
+      address ownerAddress;
+      string name;
+      int money;
+      int[] ownedHouseIds;
+    }
+
+    struct House {
+      int houseId;
+      string location;
+      int price;
+      bool isOnSale;
+      bool sold;
     }
 }
