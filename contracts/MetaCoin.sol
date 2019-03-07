fd003c69d8d35419e142a81f8d1a3625a28367d2
Fodé Guirassy <fode.guirassy@ekino.com>
2019-03-07 18:41:13 +0100

    add mock for Caroline houses

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -11,6 +11,7 @@ contract MetaCoin {
     mapping (address => uint) balances;
 
     Owner benjamin;
+    Owner caroline;
 
     mapping (address => Owner) public houseOwners;
     mapping (int => int[]) public housesToOwners;
@@ -21,6 +22,13 @@ contract MetaCoin {
     House house4;
     House house5;
 
+    House house6;
+    House house7;
+    House house8;
+    House house9;
+    House house10;
+    House house11;
+
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     constructor() public {
@@ -28,6 +36,7 @@ contract MetaCoin {
 
         //Add mocked Data
         addBenjaminHouses(0xC1991F924F713aAC384d63cdb5fdf7060C72a669, "Benjamin", 1000000);
+        addCaroHouses(0xC1991F924F713aAC384d63cdb5fdf7060C72a669, "Caroline", 3000000);
     }
 
     function addBenjaminHouses(address ownerAddr, string memory _name, int _money) public {
@@ -47,6 +56,26 @@ contract MetaCoin {
 
       houseOwners[ownerAddr] = benjamin;
     }
+
+    function addCaroHouses(address ownerAddr, string memory _name, int _money) public {
+      house1 = House(7,"24 rue de la Nation, Nice", 1420000, false, false);
+      house2 = House(8,"138 rue De Caroline, Lyon", 3160000, false, false);
+      house3 = House(9,"160 rue de Marguerite, Orleans", 147000, false, false);
+      house4 = House(10,"26 rue Marion Cotillard, Paris", 4200000, false, false);
+      house5 = House(11,"45 rue Herblé, Lyon", 250000, false, false);
+
+      int[] memory ids;
+      ids[0] = 7;
+      ids[1] = 8;
+      ids[2] = 9;
+      ids[3] = 10;
+      ids[4] = 11;
+
+      caroline = Owner(ownerAddr, _name, _money, ids);
+
+      houseOwners[ownerAddr] = caroline;
+    }
+
 
 
     function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
